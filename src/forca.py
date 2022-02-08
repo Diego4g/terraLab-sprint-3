fe1efcc80d0fc7bb070a152b340ea1a9de188449
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-08 17:00:48 -0300

    Finalizando jogo da forca

--- src/forca.py
+++ src/forca.py
@@ -3,6 +3,37 @@ def jogar():
     print("***************************")
     print("Bem vindo ao jogo de Forca!")
     print("***************************")
+
+    palavra_secreta = "banana".upper()
+    # use o caracter "_" para cada letra em palavra_secreta
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Voce ganhou !!!")
+    else:
+        print("Voce perdeu...")
     print("Fim do jogo")
 
 
